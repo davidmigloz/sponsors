6138db5636c1be338cd154258a12343d7c08eb5c
David Miguel <me@davidmiguel.com>
2023-09-01 18:55:32 +0200

    Update config

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -1,59 +1,32 @@
-import { BadgePreset, defineConfig, presets } from 'sponsorkit'
+// noinspection JSUnusedGlobalSymbols
 
-const past: BadgePreset = {
-  avatar: {
-    size: 20,
-  },
-  boxWidth: 22,
-  boxHeight: 22,
-  container: {
-    sidePadding: 35,
-  },
-}
+import { defineConfig, presets } from 'sponsorkit'
 
 export default defineConfig({
   tiers: [
     {
       title: 'Past Sponsors',
       monthlyDollars: -1,
-      preset: past,
+      preset: presets.xs,
     },
     {
       title: 'Backers',
-      preset: presets.small,
+      preset: presets.base,
     },
     {
       title: 'Sponsors',
-      monthlyDollars: 10,
-      preset: {
-        avatar: {
-          size: 42,
-        },
-        boxWidth: 52,
-        boxHeight: 52,
-        container: {
-          sidePadding: 30,
-        },
-      }
+      monthlyDollars: 5,
+      preset: presets.medium,
     },
     {
       title: 'Silver Sponsors',
       monthlyDollars: 50,
-      preset: presets.medium,
+      preset: presets.large,
     },
     {
       title: 'Gold Sponsors',
       monthlyDollars: 100,
-      preset: presets.large,
-    },
-    {
-      title: 'Platinum Sponsors',
-      monthlyDollars: 500,
       preset: presets.xl,
-    },
-    {
-      title: 'Special Sponsor',
-      monthlyDollars: Infinity,
     },
   ]
 })
